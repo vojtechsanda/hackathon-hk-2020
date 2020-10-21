f93821ec4c115f23b0cbd400121cc5ce80727719
Vojtěch Šanda <vojtech.sanda@gmail.com>
2020-10-21 11:48:39 +0200

    Added elements obj

---
+++ static/src/assets/js/views/base.js
@@ -0,0 +1,5 @@
+export const elements = {
+    searchInput: document.querySelector('.js-search-input'),
+    searchCategorySelect: document.querySelector('.js-category-select'),
+    searchFieldSelect: document.querySelector('.js-field-select'),
+};
