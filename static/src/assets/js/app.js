40c70a1e0f32f28f6095ede81ddac2f436945606
Vojtěch Šanda <vojtech.sanda@gmail.com>
2020-10-21 13:11:44 +0200

    Renamed methods & Added Search controller

--- static/src/assets/js/app.js
+++ static/src/assets/js/app.js
@@ -2,21 +2,56 @@ import '../scss/main.scss'
 
 import Axios from 'axios'
 
+import {elements} from './views/base';
+
 // Search component
 import * as searchView from './views/searchView';
 import Search from './models/Search';
 
+/**
+ * SEARCH controller
+ */
+
+class SearchController {
+    constructor(records) {
+        this.state = {
+            records: records,
+            search: new Search(records),
+        };
+    }
+    render(categories, fields) {
+        searchView.renderCategoriesSelectOptions(categories);
+        searchView.renderFieldsSelectOptions(fields);
+    }
+    getSearchedRecords() {
+        const searchedTxt = searchView.getSearchedTxt();
+        const selectedCategory = searchView.getCategory();
+        const selectedField = searchView.getField();
+
+        const searchedRecords = this.state.search.search(searchedTxt, selectedCategory, selectedField);
+
+        return searchedRecords;
+    }
+    init(categories, fields) {
+        this.render(categories, fields);
+    }
+}
+
+/**
+ * GLOBAL controller
+ */
 class Desk {
     constructor() {
         this.state = {};
+        this.controllers = {};
     }
-    async getAllRecords() {
+    async fetchAllRecords() {
         let resp;
 
         try {
             resp = await Axios('/api/api.json?records');
         } catch {
-            alert('Nebylo možné načíst data');
+            console.error('Nebylo možné načíst data');
             return false;
         }
 
@@ -24,13 +59,13 @@ class Desk {
 
         return records;
     }
-    async getAllCategories() {
+    async fetchAllCategories() {
         let resp;
 
         try {
             resp = await Axios('/api/api.json?categories');
         } catch {
-            alert('Nebylo možné kategorie data');
+            console.error('Nebylo možné kategorie data');
             return false;
         }
 
@@ -38,13 +73,13 @@ class Desk {
 
         return categories;
     }
-    async getAllFields() {
+    async fetchAllFields() {
         let resp;
 
         try {
             resp = await Axios('/api/api.json?fields');
         } catch {
-            alert('Nebylo možné načíst oblasti');
+            console.error('Nebylo možné načíst oblasti');
             return false;
         }
 
@@ -52,19 +87,45 @@ class Desk {
 
         return fields;
     }
-    async init() {
-        const records = await this.getAllRecords();
+    async getAllData() {
+        const records = await this.fetchAllRecords();
         if (records === false) return;
 
-        const categories = await this.getAllCategories();
+        const categories = await this.fetchAllCategories();
         if (categories === false) return;
 
-        const fields = await this.getAllFields();
+        const fields = await this.fetchAllFields();
         if (fields === false) return;
 
         this.state.records = records;
         this.state.categories = categories;
         this.state.fields = fields;
+
+        return true;
+    }
+    
+    setupEvents() {
+        elements.searchForm.addEventListener('submit', e => {
+            e.preventDefault();
+
+            const searchedRecords = this.controllers.search.getSearchedRecords(this.state.records);
+            this.state.searchedRecords = searchedRecords;
+            console.log(this.state.searchedRecords);
+        });
+    }
+    initControllers() {
+        this.controllers.search = new SearchController(this.state.records);
+        this.controllers.search.init(this.state.categories, this.state.fields);
+    }
+    async init() {
+        const fetchedDataStatus = await this.getAllData();
+        if (!fetchedDataStatus) {
+            alert('Nějaký zdroj se nenačtl');
+            return;
+        }
+
+        this.initControllers();
+        this.setupEvents();
     }
 }
 
