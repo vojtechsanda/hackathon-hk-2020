a0910fe445497506e4d6bbcb8137220b39f8c2e1
Vojtěch Šanda <vojtech.sanda@gmail.com>
2020-10-21 14:28:46 +0200

    Added rendering of the results

---
+++ static/src/assets/js/views/resultsView.js
@@ -0,0 +1,29 @@
+import { elements } from './base';
+
+export const render = records => {
+    const resultsWrapper = elements.resultsWrapper;
+
+    // Remove current results
+    Array.from(resultsWrapper.children).forEach(result => result.parentElement.removeChild(result));
+
+    records.forEach(record => {
+        const markup = `
+            <div>
+                <h2>${record.title}</h2>
+                <div>${record.field}</div>
+                <div>${formatDate(record.start_timestamp)}</div>
+                <div>${record.category}</div>
+            </div>
+        `;
+
+        resultsWrapper.insertAdjacentHTML('beforeend', markup);
+    });
+}
+
+function formatDate(timestamp) {
+    const d = new Date(timestamp);
+    console.log(d);
+
+    const formattedTxt = `${d.getMonth()+1}.${d.getDate()}.${d.getFullYear()}`;
+    return formattedTxt;
+}
