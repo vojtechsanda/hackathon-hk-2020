3cfaafe298f83a4b0a38f02afc6e2adde21cb7dd
Vojtěch Šanda <vojtech.sanda@gmail.com>
2020-10-21 14:28:13 +0200

    Added resultsWrapper to the elements

--- static/src/assets/js/views/base.js
+++ static/src/assets/js/views/base.js
@@ -3,4 +3,5 @@ export const elements = {
     searchInput: document.querySelector('.js-search-input'),
     searchCategorySelect: document.querySelector('.js-category-select'),
     searchFieldSelect: document.querySelector('.js-field-select'),
+    resultsWrapper: document.querySelector('.js-results-wrapper'),
 };
