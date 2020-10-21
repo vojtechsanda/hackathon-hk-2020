69a9134a47b1eb54c735714d079cf05c0990ddd0
Vojtěch Šanda <vojtech.sanda@gmail.com>
2020-10-22 00:51:40 +0200

    Removed unused method

--- static/src/assets/js/models/Results.js
+++ static/src/assets/js/models/Results.js
@@ -1,28 +1,3 @@
 export default class Results {
-    sortBy(results, sorters) {
-        const sortableResults = Array.from(results);
 
-        console.log(sortableResults, sorters);
-
-        for (const sorter of sorters) {
-            if (sorter.sortOrder === 'none') continue;
-            if (sortableResults[0] && typeof (sortableResults[0][sorter.sortBy]) === 'string') {
-                if (sorter.sortOrder === 'asc') {
-                    sortableResults.sort();
-                    sortableResults.reverse();
-                } else if (sorter.sortOrder === 'desc') {
-                    sortableResults.sort();
-                }
-            } else if (sortableResults[0] && typeof (sortableResults[0][sorter.sortBy]) === 'number') {
-                if (sorter.sortOrder === 'asc') {
-                    sortableResults.sort((result1, result2) => result1[sorter.sortBy] - result2[sorter.sortBy]);
-                } else if (sorter.sortOrder === 'desc') {
-                    sortableResults.sort((result1, result2) => result2[sorter.sortBy] - result1[sorter.sortBy]);
-                }
-            }
-
-        }
-
-        return sortableResults;
-    }
 }
